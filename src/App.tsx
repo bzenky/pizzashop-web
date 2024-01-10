cb97c0b23280d281040b33b659c8f9662de617cf
bzenky <bzenky@gmail.com>
2024-01-09 21:55:47 -0300

    feat: shadcn - button

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
+import { Button } from './components/ui/button'
 import './global.css'
 
 export function App() {
   return (
-    <h1>_nk</h1>
+    <div className='flex justify-center items-center h-screen'>
+      <Button>
+        _nk
+      </Button>
+    </div>
   )
 }
