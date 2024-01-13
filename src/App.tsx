1b1428a19643faed4ba9a662eb6053c7f88ca836
bzenky <bzenky@gmail.com>
2024-01-13 18:38:32 -0300

    feat: toggle app theme

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,18 @@ import './global.css'
 import { Helmet, HelmetProvider } from 'react-helmet-async'
 import { RouterProvider } from 'react-router-dom'
 
+import { ThemeProvider } from './components/theme/theme-provider'
 import { Toaster } from './components/ui/sonner'
 import { router } from './routes'
 
 export function App() {
   return (
     <HelmetProvider>
-      <Helmet titleTemplate="%s | pizza.shop" />
-      <RouterProvider router={router} />
-      <Toaster richColors />
+      <ThemeProvider storageKey="pizzashop-theme" defaultTheme="dark">
+        <Helmet titleTemplate="%s | pizza.shop" />
+        <RouterProvider router={router} />
+        <Toaster richColors />
+      </ThemeProvider>
     </HelmetProvider>
   )
 }
