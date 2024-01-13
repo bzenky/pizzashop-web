f767f2bb4eb525ad89924af0b2167a33b12d0c76
bzenky <bzenky@gmail.com>
2024-01-12 21:53:25 -0300

    feat: static sign up page

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,5 +1,6 @@
 import { Helmet } from 'react-helmet-async'
 import { useForm } from 'react-hook-form'
+import { Link } from 'react-router-dom'
 import { toast } from 'sonner'
 import { z } from 'zod'
 
@@ -36,7 +37,11 @@ export function SignIn() {
     <>
       <Helmet title="Login" />
       <div className="p-8">
-        <div className="w[350px] flex flex-col justify-center gap-6">
+        <Button variant="ghost" asChild className="absolute right-8 top-8">
+          <Link to="/sign-up">Novo estabelecimento</Link>
+        </Button>
+
+        <div className="flex w-[350px] flex-col justify-center gap-6">
           <div className="flex flex-col gap-2 text-center tracking-tight">
             <h1 className="text-2xl font-semibold">Acessar painel</h1>
             <p className="text-sm text-muted-foreground">
