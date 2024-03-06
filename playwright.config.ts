6fb8c9fd5802e0d0ceac257e2fe7c4b1b0110256
bzenky <bzenky@gmail.com>
2024-03-05 21:30:50 -0300

    feat: test e2e: sign-in

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,13 +2,14 @@ import { defineConfig } from '@playwright/test'
 
 export default defineConfig({
   testDir: './test',
+  testMatch: /.*\.e2e-spec\.ts$/,
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
   reporter: 'html',
   use: {
-    // baseURL: 'http://localhost:50789',
+    baseURL: 'http://localhost:50789',
   },
   webServer: {
     command: 'pnpm dev:test',
