decc25c70f997980df8694efddbc19c76af3f919
bzenky <bzenky@gmail.com>
2024-03-05 22:10:52 -0300

    feat: test e2e: update-profile

---
+++ test/store-profile.e2e-spec.ts
@@ -0,0 +1,40 @@
+import { expect, test } from '@playwright/test'
+
+test('sign up successfully', async ({ page }) => {
+  await page.goto('/', { waitUntil: 'networkidle' })
+
+  await page.getByRole('button', { name: 'Pizza Shop' }).click()
+  await page.getByRole('menuitem', { name: 'Perfil da loja' }).click()
+
+  await page.getByLabel('Nome').fill('Xablau')
+  await page.getByLabel('Descrição').fill('Lorem Ipsum')
+
+  await page.getByRole('button', { name: 'Salvar' }).click()
+
+  await page.waitForLoadState('networkidle')
+
+  const toast = page.getByText('Perfil atualizado com sucesso.')
+  expect(toast).toBeVisible()
+
+  await page.getByRole('button', { name: 'Close' }).click()
+  const updatedName = page.getByRole('button', { name: 'Xablau' })
+  expect(updatedName).toBeVisible()
+})
+
+test('sign up with error', async ({ page }) => {
+  await page.goto('/', { waitUntil: 'networkidle' })
+
+  await page.getByRole('button', { name: 'Pizza Shop' }).click()
+  await page.getByRole('menuitem', { name: 'Perfil da loja' }).click()
+
+  await page.getByLabel('Nome').fill('Error Response')
+  await page.getByLabel('Descrição').fill('Lorem Ipsum')
+
+  await page.getByRole('button', { name: 'Salvar' }).click()
+
+  await page.waitForLoadState('networkidle')
+
+  const toast = page.getByText('Falha ao atualizar o perfil, tente novamente!')
+
+  expect(toast).toBeVisible()
+})
